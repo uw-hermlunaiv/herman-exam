e818a43545f19c61b071fb1944c9e56dc8ab603f
Herman Luna <upwork.hermanluna@gmail.com>
2022-03-09 19:55:56 +0800

    fix: interface on orders api

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -1,9 +1,15 @@
 import axios from '.';
+import {
+  IOrderItem,
+} from '../store/reducers/order.reducer';
 
 const resource = '/client/orders';
 
 const ordersAPI = {
-  getOrders: (limit = 100, offset = 0): Promise<any> => axios.get(`${resource}?limit=${limit}&offset=${offset}`),
+  getOrders: (limit = 100, offset = 0): Promise<{
+    Items: IOrderItem[],
+    Count: number
+  }> => axios.get(`${resource}?limit=${limit}&offset=${offset}`),
   createOrders: (body: any) => axios.post(resource, body),
   updateOrders: (body: any) => axios.post(resource, body),
   deleteOrders: (id: number) => axios.delete(`${resource}/${id}`),
